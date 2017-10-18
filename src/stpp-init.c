a0cfc5c490f48afe54c748fb5595d29bfca061f4
frajaroco <frajaroco@gmail.com>
2017-10-18 13:07:48 +0200

    Comment 18-10-2017-2

--- src/stpp-init.c
+++ src/stpp-init.c
@@ -1,7 +1,26 @@
 #include <R_ext/RS.h>
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
-#include "stpp-init.h"
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .Fortran calls */
+extern void F77_NAME(astk)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(circ)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(covst)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(pcffunction)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(stikfunction)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+static const R_FortranMethodDef FortranEntries[] = {
+    {"astk",         (DL_FUNC) &F77_NAME(astk),         15},
+    {"circ",         (DL_FUNC) &F77_NAME(circ),         12},
+    {"covst",        (DL_FUNC) &F77_NAME(covst),        13},
+    {"pcffunction",  (DL_FUNC) &F77_NAME(pcffunction),  23},
+    {"stikfunction", (DL_FUNC) &F77_NAME(stikfunction), 20},
+    {NULL, NULL, 0}
+};
 
 void R_init_stpp(DllInfo *dll)
 {
