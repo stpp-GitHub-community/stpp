fa783d8ce06313e5d204ac1622df23ac4bafe996
frajaroco <frajaroco@gmail.com>
2017-11-09 10:46:05 +0100

    Comment 9-11-2017-3

--- src/stpp-init.h
+++ src/stpp-init.h
@@ -5,12 +5,14 @@
 /* .Fortran calls */
 extern void F77_NAME(circ)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(covst)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(listafunction)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(pcffunction)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void F77_NAME(stikfunction)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 
 static const R_FortranMethodDef FortranEntries[] = {
   {"circ",         (DL_FUNC) &F77_NAME(circ),         12},
   {"covst",        (DL_FUNC) &F77_NAME(covst),        13},
+  {"listafunction", (DL_FUNC) &F77_NAME(listafunction), 27},
   {"pcffunction",  (DL_FUNC) &F77_NAME(pcffunction),  23},
   {"stikfunction", (DL_FUNC) &F77_NAME(stikfunction), 20},
   {NULL, NULL, 0}
