3ede2151417d9d25839488e2ca6a60a2e3d13962
frajaroco <frajaroco@gmail.com>
2017-11-27 11:08:57 +0100

    Comment 27-11-2017-3

---
+++ src/init.c
@@ -0,0 +1,32 @@
+#include <R_ext/RS.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+Check these declarations against the C/Fortran source code.
+*/
+
+/* .Fortran calls */
+extern void F77_NAME(astk)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(circ)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(covst)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(listafunction)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(pcffunction)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(stikfunction)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+static const R_FortranMethodDef FortranEntries[] = {
+  {"astk",          (DL_FUNC) &F77_NAME(astk),          15},
+  {"circ",          (DL_FUNC) &F77_NAME(circ),          12},
+  {"covst",         (DL_FUNC) &F77_NAME(covst),         13},
+  {"listafunction", (DL_FUNC) &F77_NAME(listafunction), 27},
+  {"pcffunction",   (DL_FUNC) &F77_NAME(pcffunction),   23},
+  {"stikfunction",  (DL_FUNC) &F77_NAME(stikfunction),  20},
+  {NULL, NULL, 0}
+};
+
+void R_init_stpp(DllInfo *dll)
+{
+  R_registerRoutines(dll, NULL, NULL, FortranEntries, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+  R_forceSymbols(dll, TRUE);
+}
