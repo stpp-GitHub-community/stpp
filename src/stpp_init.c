d1f2da1f6a3aae0692507f0d21e8d5a5dc3c6023
frajaroco <frajaroco@gmail.com>
2017-11-27 13:51:48 +0100

    Comment 27-11-2017-4

--- src/stpp_init.c
+++ src/stpp_init.c
@@ -28,5 +28,5 @@ void R_init_stpp(DllInfo *dll)
 {
   R_registerRoutines(dll, NULL, NULL, FortranEntries, NULL);
   R_useDynamicSymbols(dll, FALSE);
-  R_forceSymbols(dll, TRUE);
+  //R_forceSymbols(dll, TRUE);
 }
