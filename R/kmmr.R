47759f5f72c4b45802656a1dfa4f489b79cbe6c1
Francisco J. Rodríguez-Cortés <frajaroco@gmail.com>
2024-07-18 08:04:36 -0500

    spatial dist

--- R/kmmr.R
+++ R/kmmr.R
@@ -80,10 +80,7 @@ kmmr <- function(xyt,s.region,s.lambda,ds,ks="epanech",hs,correction="none",appr
   if (missing(ds)){
     rect <- as.rectangle(bsw)
     maxd <- min(diff(rect$xrange),diff(rect$yrange))/4
-    ds <- seq(hs, maxd,len=100)[-1]
-    ds <- sort(ds)
-  }
-  if(ds[1]==0){ds <- ds[-1]
+    ds <- seq(0, maxd,len=100)
   }
   
   kernel <- c(ks=ks,hs=hs)
